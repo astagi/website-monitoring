ab5aaea6e8eb782823f4c290ea6d9ebff42f7f6c
Andrea Stagi <stagi.andrea@gmail.com>
2021-02-18 09:47:20 +0100

    chore: black formatting

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -5,44 +5,53 @@ from kafka import KafkaConsumer
 
 
 class Consumer:
-
     def connect(self):
         self._db = psycopg2.connect(
-            host=os.getenv('POSTGRES_HOST', 'localhost'),
-            dbname=os.getenv('POSTGRES_DB', 'postgres'),
-            user=os.getenv('POSTGRES_USER', 'postgres'),
-            password=os.getenv('POSTGRES_PASSWORD', 'postgres')
+            host=os.getenv("POSTGRES_HOST", "localhost"),
+            dbname=os.getenv("POSTGRES_DB", "postgres"),
+            user=os.getenv("POSTGRES_USER", "postgres"),
+            password=os.getenv("POSTGRES_PASSWORD", "postgres"),
         )
         self._db_cur = self._db.cursor()
         self._db_cur.execute("DROP TABLE website_status;")
         self._db.commit()
-        self._db_cur.execute("""
+        self._db_cur.execute(
+            """
             CREATE TABLE website_status (id serial PRIMARY KEY, url varchar unique, status_code integer, content_check boolean, time decimal, report_time timestamp);
-        """)
+        """
+        )
         self._db.commit()
         self._consumer = None
         try:
             self._consumer = KafkaConsumer(
-                'report',
-                auto_offset_reset='earliest',
+                "report",
+                auto_offset_reset="earliest",
                 bootstrap_servers=[f"{os.getenv('KAFKA_HOST', 'localhost')}:9092"],
                 api_version=(0, 10),
             )
         except Exception as ex:
-            print('Exception while connecting Kafka')
+            print("Exception while connecting Kafka")
             print(str(ex))
 
     def start(self):
         for msg in self._consumer:
             site_stat = json.loads(msg.value)
-            print (msg.value)
-            self._db_cur.execute("""
+            print(msg.value)
+            self._db_cur.execute(
+                """
                 INSERT INTO website_status (url, status_code, content_check, time, report_time)
                 VALUES (%s, %s, %s, %s, %s)
                 ON CONFLICT ON CONSTRAINT website_status_url_key
                 DO UPDATE SET status_code=excluded.status_code, content_check=excluded.content_check, time=excluded.time, report_time=excluded.report_time;
             """,
-            (msg.key.decode('utf-8'), site_stat["status_code"], site_stat["content_check"], site_stat["time"], site_stat["report_time"]))
+                (
+                    msg.key.decode("utf-8"),
+                    site_stat["status_code"],
+                    site_stat["content_check"],
+                    site_stat["time"],
+                    site_stat["report_time"],
+                ),
+            )
             self._db.commit()
         self._consumer.close()
         self._db_cur.close()
--- producer/producer.py
+++ producer/producer.py
@@ -2,49 +2,50 @@ import os
 import json
 import requests
 import time
-
 import datetime
 from kafka import KafkaProducer
 
 
 class Producer:
-
     def _send_report(self, url, content_check, status_code, time):
-        print (url)
-        print (status_code)
-        print (time)
+        print(url)
+        print(status_code)
+        print(time)
         try:
-            url_bytes = bytes(url, encoding='utf-8')
-            body_bytes = bytes(json.dumps({
-                "url": url,
-                "status_code": status_code,
-                "content_check": content_check,
-                "time": time,
-                "report_time": datetime.datetime.now().isoformat()
-            }), encoding='utf-8')
-            self._producer_instance.send('report', key=url_bytes, value=body_bytes)
+            url_bytes = bytes(url, encoding="utf-8")
+            body_bytes = bytes(
+                json.dumps(
+                    {
+                        "url": url,
+                        "status_code": status_code,
+                        "content_check": content_check,
+                        "time": time,
+                        "report_time": datetime.datetime.now().isoformat(),
+                    }
+                ),
+                encoding="utf-8",
+            )
+            self._producer_instance.send("report", key=url_bytes, value=body_bytes)
             self._producer_instance.flush()
-            print('Message published successfully.')
+            print("Message published successfully.")
         except Exception as ex:
-            print('Exception in publishing message')
+            print("Exception in publishing message")
             print(str(ex))
-
 
     def connect(self):
         self._producer_instance = None
         try:
             self._producer_instance = KafkaProducer(
                 bootstrap_servers=[f"{os.getenv('KAFKA_HOST', 'localhost')}:9092"],
-                api_version=(0, 10)
+                api_version=(0, 10),
             )
         except Exception as ex:
-            print('Exception while connecting Kafka')
+            print("Exception while connecting Kafka")
             print(str(ex))
-
 
     def start(self):
         config = []
-        with open('./config.json', 'r') as f:
+        with open("./config.json", "r") as f:
             config = json.loads(f.read())
         while True:
             for website in config:
